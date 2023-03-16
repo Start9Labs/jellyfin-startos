530f74530f6e4c41a3f2e71c758f3e1b9cd365f1
Shadowy Super Coder <musashidisciple@proton.me>
2023-03-15 21:35:23 -0600

    Increase health check duration

--- scripts/deps.ts
+++ scripts/deps.ts
@@ -1,3 +1,3 @@
 export * from "https://deno.land/x/embassyd_sdk@v0.3.3.0.9/mod.ts";
-// export * from "https://deno.land/x/embassyd_sdk@v0.3.3.0.9/util.ts";
-// export * from "https://deno.land/x/embassyd_sdk@v0.3.3.0.9/healthUtil.ts";
+export * from "https://deno.land/x/embassyd_sdk@v0.3.3.0.9/util.ts";
+export * from "https://deno.land/x/embassyd_sdk@v0.3.3.0.9/healthUtil.ts";
--- scripts/procedures/health.ts
+++ scripts/procedures/health.ts
@@ -1,41 +1,8 @@
-// import { types as T } from "../deps.ts";
-
-// export const health: T.ExpectedExports.health = {
-//     // deno-lint-ignore require-await
-//     async "web"(effects, duration) {
-//         return healthWeb(effects, duration);
-//     },
-// };
-
-// const healthWeb: T.ExpectedExports.health[""] = async (effects, duration) => {
-//     await guardDurationAboveMinimum({ duration, minimumTime: 30000 });
-
-//     return await effects.fetch("http://jellyfin.embassy:8096")
-//         .then((_) => ok)
-//         .catch((e) => {
-//             effects.error(`${e}`)
-//             return error(`The Jellyfin Web UI is unreachable`)
-//         });
-// };
-
-// // *** HELPER FUNCTIONS *** //
-
-// // Ensure the starting duration is past a minimum
-// const guardDurationAboveMinimum = (
-//     input: { duration: number; minimumTime: number },
-// ) =>
-//     (input.duration <= input.minimumTime)
-//         ? Promise.reject(errorCode(60, "Starting"))
-//         : null;
-
-// const errorCode = (code: number, error: string) => ({
-//     "error-code": [code, error] as const,
-// });
-// const error = (error: string) => ({ error });
-// const ok = { result: null };
-
-import { types as T, healthUtil } from "../deps.ts";
+import { types as T, checkWebUrl, catchError } from "../deps.ts";
 
 export const health: T.ExpectedExports.health = {
-  "alive": healthUtil.checkWebUrl("http://jellyfin.embassy:8096")
+    // deno-lint-ignore require-await
+    async "alive"(effects, duration) {
+      return checkWebUrl("http://jellyfin.embassy:8096")(effects, 30000).catch(catchError(effects))
+    },
 };
