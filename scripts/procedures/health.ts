c3de2ccf74e40cd08b74d9e540c7b20cdf7e771b
Shadowy Super Coder <musashidisciple@proton.me>
2023-03-21 09:59:56 -0600

    Remove unused commented out code

--- scripts/procedures/health.ts
+++ scripts/procedures/health.ts
@@ -1,12 +1,3 @@
-// import { types as T, checkWebUrl, catchError } from "../deps.ts";
-
-// export const health: T.ExpectedExports.health = {
-//     // deno-lint-ignore require-await
-//     async "alive"(effects, duration) {
-//       return checkWebUrl("http://jellyfin.embassy:8096")(effects, 90000).catch(catchError(effects))
-//     },
-// };
-
 import { types as T, healthUtil } from "../deps.ts";
 
 export const health: T.ExpectedExports.health = {
