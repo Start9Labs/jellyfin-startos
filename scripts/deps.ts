659452a1757ae0990e9e4d4f52d8f519a2d7ec51
Mariusz Kogen <k0gen@pm.me>
2024-06-22 14:21:08 +0300

    Code cleanup

--- scripts/deps.ts
+++ scripts/deps.ts
@@ -1,3 +1,1 @@
 export * from "https://deno.land/x/embassyd_sdk@v0.3.3.0.9/mod.ts";
-export * from "https://deno.land/x/embassyd_sdk@v0.3.3.0.9/util.ts";
-export * from "https://deno.land/x/embassyd_sdk@v0.3.3.0.9/healthUtil.ts";
