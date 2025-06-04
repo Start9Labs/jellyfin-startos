e1f72604ef8d45461811b7b4df67e4054ba0daac
Shadowy Super Coder <musashidisciple@proton.me>
2025-06-04 13:38:39 -0600

    write config.json on preInstall

--- startos/install/versionGraph.ts
+++ startos/install/versionGraph.ts
@@ -1,8 +1,12 @@
 import { VersionGraph } from '@start9labs/start-sdk'
 import { current, other } from './versions'
+import { configJson } from '../fileModels/config.json'
+import { configDefaults } from '../utils'
 
 export const versionGraph = VersionGraph.of({
   current,
   other,
-  preInstall: async (effects) => {},
+  preInstall: async (effects) => {
+    await configJson.write(effects, configDefaults)
+  },
 })
