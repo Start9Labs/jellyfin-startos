7b798be0385280586357da82329bfbde984d7685
Mariusz Kogen <k0gen@pm.me>
2025-07-08 17:48:51 +0200

    update for sdk beta 32

--- startos/main.ts
+++ startos/main.ts
@@ -1,5 +1,4 @@
 import { sdk } from './sdk'
-import { T } from '@start9labs/start-sdk'
 import { uiPort } from './utils'
 import { store } from './fileModels/store.json'
 import { manifest as filebrowserManifest } from 'filebrowser-startos/startos/manifest'
@@ -55,13 +54,6 @@ export const main = sdk.setupMain(async ({ effects, started }) => {
   }
 
   /**
-   * ======================== Additional Health Checks (optional) ========================
-   *
-   * In this section, we define *additional* health checks beyond those included with each daemon (below).
-   */
-  const additionalChecks: T.HealthCheck[] = []
-
-  /**
    * ======================== Daemons ========================
    *
    * In this section, we create one or more daemons that define the service runtime.
@@ -69,35 +61,32 @@ export const main = sdk.setupMain(async ({ effects, started }) => {
    * Each daemon defines its own health check, which can optionally be exposed to the user.
    */
 
-  return sdk.Daemons.of(effects, started, additionalChecks).addDaemon(
-    'primary',
-    {
-      subcontainer: await sdk.SubContainer.of(
-        effects,
-        { imageId: 'jellyfin' },
-        mounts,
-        'jellyfin-sub',
-      ),
-      exec: {
-        command: [
-          'jellyfin/jellyfin',
-          '--ffmpeg',
-          '/usr/lib/jellyfin-ffmpeg/ffmpeg',
-        ],
-      },
-      ready: {
-        display: 'Web Interface',
-        fn: () =>
-          sdk.healthCheck.checkWebUrl(
-            effects,
-            `http://localhost:${uiPort}/health`,
-            {
-              successMessage: 'The web interface is ready',
-              errorMessage: 'The web interface is not ready',
-            },
-          ),
-      },
-      requires: [],
+  return sdk.Daemons.of(effects, started).addDaemon('primary', {
+    subcontainer: await sdk.SubContainer.of(
+      effects,
+      { imageId: 'jellyfin' },
+      mounts,
+      'jellyfin-sub',
+    ),
+    exec: {
+      command: [
+        'jellyfin/jellyfin',
+        '--ffmpeg',
+        '/usr/lib/jellyfin-ffmpeg/ffmpeg',
+      ],
     },
-  )
+    ready: {
+      display: 'Web Interface',
+      fn: () =>
+        sdk.healthCheck.checkWebUrl(
+          effects,
+          `http://localhost:${uiPort}/health`,
+          {
+            successMessage: 'The web interface is ready',
+            errorMessage: 'The web interface is not ready',
+          },
+        ),
+    },
+    requires: [],
+  })
 })
