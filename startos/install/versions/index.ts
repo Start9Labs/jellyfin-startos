cda27415ccf0582dc784088b2323d542e93c3edd
Mariusz Kogen <k0gen@pm.me>
2025-07-31 15:28:58 +0200

    add migration

--- startos/install/versions/index.ts
+++ startos/install/versions/index.ts
@@ -1,2 +1,4 @@
-export { v_10_10_7_1 as current } from './v10.10.7.1'
-export const other = []
+export { v_10_10_7_2 as current } from './v10.10.7.2-alpha.0'
+import { v_10_10_7_1 } from './v10.10.7.1'
+
+export const other = [v_10_10_7_1]
