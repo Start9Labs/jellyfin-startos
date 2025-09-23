3dde0c16e798a60b3dec9dee509f55681ed0d0c0
Mariusz Kogen <k0gen@pm.me>
2025-09-23 08:18:20 +0200

    Update Makefile, sdk.beta.41, devDeps and other

--- startos/manifest.ts
+++ startos/manifest.ts
@@ -1,4 +1,10 @@
 import { setupManifest } from '@start9labs/start-sdk'
+import { SDKImageInputSpec } from '@start9labs/start-sdk/base/lib/types/ManifestTypes'
+
+const BUILD = process.env.BUILD || ''
+
+const architectures =
+  BUILD === 'x86_64' || BUILD === 'aarch64' ? [BUILD] : ['x86_64', 'aarch64']
 
 export const manifest = setupManifest({
   id: 'jellyfin',
@@ -10,7 +16,7 @@ export const manifest = setupManifest({
   marketingSite: 'https://jellyfin.org',
   donationUrl: 'https://opencollective.com/jellyfin/donate',
   docsUrl:
-    'https://github.com/Start9Labs/jellyfin-startos/blob/master/instructions.md',
+    'https://github.com/Start9Labs/jellyfin-startos/blob/update/040/docs/README.md',
   description: {
     short: 'The Free Software Media System',
     long: 'Jellyfin is a free and open source media server that enables you to organize, manage, and stream your personal media collection to any device. It serves as a community-driven alternative to proprietary platforms like Emby and Plex, offering full control over your media without tracking or licensing restrictions.',
@@ -21,21 +27,27 @@ export const manifest = setupManifest({
       source: {
         dockerTag: 'jellyfin/jellyfin:10.10.7',
       },
-    },
+      arch: architectures
+    } as SDKImageInputSpec,
   },
-  hardwareRequirements: {},
+  hardwareRequirements: { arch: architectures},
   alerts: {},
   dependencies: {
     filebrowser: {
       description: 'Used to get media from File Browser',
       optional: true,
-      s9pk: 'https://github.com/Start9Labs/filebrowser-startos/releases/download/v2.32.0/filebrowser_040_alpha.s9pk',
+      metadata: {
+        title: 'File Browser',
+        icon: 'https://raw.githubusercontent.com/Start9Labs/filebrowser-startos/refs/heads/master/icon.png',
+        },
     },
     nextcloud: {
       description: 'Used to get media from Nextcloud',
       optional: true,
-      // TODO update me
-      s9pk: 'https://github.com/Start9Labs/nextcloud-startos/releases/download/v29.0.14.1/nextcloudV2.s9pk',
+      metadata: {
+        title: 'Nextcloud',
+        icon: 'https://raw.githubusercontent.com/Start9Labs/nextcloud-startos/refs/heads/master/icon.png',
+      },
     },
   },
 })
