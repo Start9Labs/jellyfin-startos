4ba2f3a3b9d26bc6396f55ebe84e8a09de17c700
Mariusz Kogen <k0gen@pm.me>
2025-11-21 18:11:11 +0100

    sdk.beta.44 bump

--- startos/install/versions/v10.11.2.1.ts
+++ startos/install/versions/v10.11.2.1.ts
@@ -1,7 +1,7 @@
 import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
 
 export const v_10_11_2_1 = VersionInfo.of({
-  version: '10.11.2:1-alpha.0',
+  version: '10.11.2:1-alpha.1',
   releaseNotes: `\
   Updated for StartOS 0.4.0`,
   migrations: {
