78e4648b9214209985e19e76568c65570c630a6f
vincent <vincent@athena.powerhub>
2026-04-09 12:05:01 +0100

    Updated Jellyfin version

--- startos/versions/index.ts
+++ startos/versions/index.ts
@@ -1,7 +1,7 @@
 import { VersionGraph } from '@start9labs/start-sdk'
-import { v_10_11_7_0 } from './v10.11.7.0'
+import { v_10_11_8_0 } from './v10.11.8.0'
 
 export const versionGraph = VersionGraph.of({
-  current: v_10_11_7_0,
+  current: v_10_11_8_0,
   other: [],
 })
---
+++ startos/versions/v10.11.8.0.ts
@@ -0,0 +1,48 @@
+import { VersionInfo, IMPOSSIBLE, YAML } from '@start9labs/start-sdk'
+import { readFile, rm } from 'fs/promises'
+import { configJson, defaultPlugins } from '../fileModels/config.json'
+import { store, type StoreType } from '../fileModels/store.json'
+
+export const v_10_11_8_0 = VersionInfo.of({
+  version: '10.11.8:0',
+  releaseNotes: {
+    en_US: 'Update Jellyfin to 10.11.8',
+    es_ES: 'Actualización de Jellyfin a 10.11.8',
+    de_DE: 'Update von Jellyfin auf 10.11.8',
+    pl_PL: 'Aktualizacja Jellyfin do 10.11.8',
+    fr_FR: 'Mise à jour de Jellyfin vers 10.11.8',
+  },
+  migrations: {
+    up: async ({ effects }) => {
+      // get old config.yaml
+      const configYaml:
+        | {
+            mediasources: StoreType['mediaSources']
+            chromecast: boolean
+            trailers: boolean
+          }
+        | undefined = await readFile(
+        '/media/startos/volumes/main/start9/config.yaml',
+        'utf-8',
+      ).then(YAML.parse, () => undefined)
+
+      if (configYaml) {
+        await store.write(effects, {
+          mediaSources: configYaml.mediasources || [],
+        })
+
+        const plugins = [...defaultPlugins]
+        if (configYaml.chromecast) plugins.push('chromecast')
+        if (configYaml.trailers) plugins.push('trailers')
+
+        await configJson.merge(effects, { plugins })
+
+        // remove old start9 dir
+        await rm('/media/startos/volumes/main/start9', {
+          recursive: true,
+        }).catch(console.error)
+      }
+    },
+    down: IMPOSSIBLE,
+  },
+})
